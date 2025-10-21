3e8b74a13a6885e28b63c2d73161cca524904dc7
komed3 <pk97@outlook.de>
2025-10-21 18:58:44 +0200

    Create atomics.ts

---
+++ types/collection/atomics.ts
@@ -0,0 +1,34 @@
+import { Collection, Distinct, Group, Single } from '../abstract/collection';
+import { ShellModel } from '../abstract/const';
+import { ArrayProperty, NumberProperty, PrimitiveProperty } from '../abstract/property';
+
+export type AtomicsCollection = Collection< {
+
+    // Basic atomic properties
+    atomicNumber?: Distinct< number >;
+    massNumber?: Distinct< number >;
+
+    // Electron configuration
+    electronConfiguration?: Single< PrimitiveProperty< string > >;
+    shellModel?: Distinct< { [ K in ShellModel ]?: number } >;
+
+    // Ionization energy
+    ionizationEnergies?: Single< ArrayProperty< 'energy' > >;
+
+    // Atomic mass
+    mass?: Group< {
+        atomicMass?: Single< NumberProperty< 'mass' > >;
+        standardAtomicWeight?: Single< PrimitiveProperty< number > >;
+        relativeAtomicMass?: Single< PrimitiveProperty< number > >;
+        massExcess?: Single< NumberProperty< 'energy' > >;
+    } >;
+
+    // Atomic radius
+    radius?: Group< {
+        empiricalRadius?: Single< NumberProperty< 'length' > >;
+        calculatedRadius?: Single< NumberProperty< 'length' > >;
+        covalentRadius?: Single< NumberProperty< 'length' > >;
+        vdwRadius?: Single< NumberProperty< 'length' > >;
+    } >;
+
+} >;
