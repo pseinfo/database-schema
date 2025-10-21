581266ea8cc8c20c8992d7e9ab14597bec05aafd
komed3 <pk97@outlook.de>
2025-10-21 15:59:38 +0200

    Update chemistry.ts

--- types/collection/chemistry.ts
+++ types/collection/chemistry.ts
@@ -1,5 +1,5 @@
-import { Collection, Group, Single } from '../abstract/collection';
-import { BasicityCharacter, HSAB, LewisModule } from '../abstract/const';
+import { Collection, Distinct, Group, Single } from '../abstract/collection';
+import { BasicityCharacter, BondHybrid, BondType, HSAB, LewisModule, OxideCharacter } from '../abstract/const';
 import { NumberProperty, PrimitiveProperty } from '../abstract/property';
 import { CrystalSystemGroup } from './generic';
 
@@ -45,10 +45,32 @@ export type ChemistryCollection = Collection< {
         electrochemicalEquivalent?: Single< NumberProperty< 'mass' > >;
     } >;
 
+    // Oxidation properties
+    oxidation?: Group< {
+        oxidationStates?: Distinct< string[] >;
+        oxideCharacter?: Single< PrimitiveProperty< OxideCharacter > >;
+    } >;
+
+    // Bonding
+    bonding?: Group< {
+        bondType?: Single< PrimitiveProperty< BondType > >;
+        bondOrder?: Single< PrimitiveProperty< number > >;
+        bondLength?: Single< NumberProperty< 'length' > >;
+        bondEnergy?: Single< NumberProperty< 'energy' > >;
+        bondAngle?: Single< NumberProperty< 'angle' > >;
+        hybridization?: Single< PrimitiveProperty< BondHybrid > >;
+    } >;
+
     // Solubility
     solubility?: Group< {
         waterSolubility?: Single< NumberProperty< 'concentration' > >;
         henryConstant?: Single< NumberProperty< 'pressure' > >;
+    } >;
+
+    // Thermochemical properties
+    thermochemistry?: Group< {
+        standardGibbsEnergy?: Single< NumberProperty< 'energy' > >;
+        bindingEnergy?: Single< NumberProperty< 'energy' > >;
     } >;
 
 } >;
