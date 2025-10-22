8339deba66488c533cfb8d100612a825011e815a
komed3 <pk97@outlook.de>
2025-10-22 23:12:55 +0200

    index nuclides by their element symbol

--- types/nuclides/index.ts
+++ types/nuclides/index.ts
@@ -3,9 +3,16 @@
  * Defines the collection for nuclides (isotopes of elements).
  */
 
+import { Collection } from '../abstract/collection';
 import { MetaData } from '../collection/generic';
+import { ElementSymbol } from '../element';
 
-// Collection type for all nuclides, indexed by their unique string identifiers
+// Type for a single nuclide entry (all properties)
+type SingleNuclide = Collection< {} >;
+
+// Collection type for all nuclides, indexed by their element symbol
 export type NuclideCollection = {
-    [ K in string ]: MetaData & {};
+    [ E in ElementSymbol ]?: MetaData & {
+        nuclides: Record< string, SingleNuclide >;
+    };
 };
