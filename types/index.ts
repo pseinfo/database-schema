8403ae1dda4d9b3a4d1ea7c3df013685d9282055
komed3 <pk97@outlook.de>
2025-10-22 23:13:33 +0200

    update the DataBase type

--- types/index.ts
+++ types/index.ts
@@ -6,9 +6,11 @@
 import { ReferenceCollection } from './abstract/reference';
 import { UnitCollection } from './abstract/unit';
 import { ElementCollection } from './element';
+import { NuclideCollection } from './nuclides';
 
 export type DataBase = {
     elements: ElementCollection;
+    nuclides: NuclideCollection;
     references: ReferenceCollection;
     units: UnitCollection;
 };
