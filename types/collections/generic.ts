c8e0fd31387c76a6dde5375334165f281e841209
komed3 <pk97@outlook.de>
2025-11-26 23:59:48 +0100

    Update generic.ts

--- types/collections/generic.ts
+++ types/collections/generic.ts
@@ -1,5 +1,14 @@
+/**
+ * Generic Collection Types
+ * Defined common collection types for various data groups.
+ */
+
 import { Distinct } from '../abstract/collection';
 
+/**
+ * MetaData Type
+ * Represents metadata information for collections.
+ */
 export type MetaData = Distinct< {
     "@metadata": {
         schemaVersion: 1;
