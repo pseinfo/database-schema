bc7dca6dda5c0164d6ab7d8e6863ddfa3cfdace7
komed3 <pk97@outlook.de>
2025-11-26 13:23:02 +0100

    implement specific reference types

--- types/abstract/reference.ts
+++ types/abstract/reference.ts
@@ -4,7 +4,7 @@
  */
 
 import { Brand } from 'devtypes/types/base';
-import { StrictSubset } from 'devtypes/types/constraints';
+import { ExtractFrom, RequireAtLeastOne, RequireExactlyOne, StrictSubset } from 'devtypes/types/constraints';
 
 /** Reference types based on BibTeX */
 export const ReferenceType = [ 'article', 'book', 'booklet', 'conference', 'inbook', 'incollection', 'inproceedings', 'manual', 'mastersthesis', 'thesis', 'misc', 'phdthesis', 'proceedings', 'techreport', 'unpublished' ] as const;
@@ -52,16 +52,75 @@ interface BibTeXFields {
 }
 
 /** Helper types for specific reference types */
-type Thesis< T extends 'mastersthesis' | 'thesis' | 'phdthesis' > = BaseFields< T > & StrictSubset<
-    BibTeXFields,
+type Thesis< T extends 'mastersthesis' | 'thesis' | 'phdthesis' > = BaseFields< T > & StrictSubset< BibTeXFields,
     'author' | 'title' | 'school' | 'year',
     'type' | 'address' | 'month' | 'note'
 >;
 
-type Conference< T extends 'conference' | 'inproceedings' > = BaseFields< T > & StrictSubset<
-    BibTeXFields,
+type Conference< T extends 'conference' | 'inproceedings' > = BaseFields< T > & StrictSubset< BibTeXFields,
     'author' | 'title' | 'booktitle' | 'year',
     'editor' | 'volume' | 'number' | 'series' | 'pages' | 'address' | 'month' | 'organization' | 'publisher' | 'note'
 >;
 
+/** Specific reference types */
+export type ArticleReference = BaseFields< 'article' > & StrictSubset< BibTeXFields,
+    'author' | 'title' | 'journal' | 'year',
+    'volume' | 'number' | 'pages' | 'month' | 'note'
+>;
 
+export type BookReference = BaseFields< 'book' > & RequireExactlyOne< BibTeXFields, 'author' | 'editor' > & StrictSubset< BibTeXFields,
+    'title' | 'publisher' | 'year',
+    'volume' | 'number' | 'series' | 'address' | 'edition' | 'month' | 'note' | 'isbn'
+>;
+
+export type BookletReference = BaseFields< 'booklet' > & StrictSubset< BibTeXFields,
+    'title',
+    'author' | 'howpublished' | 'address' | 'month' | 'year' | 'note'
+>;
+
+export type ConferenceReference = Conference< 'conference' >;
+
+export type InbookReference = BaseFields< 'inbook' > & RequireExactlyOne< BibTeXFields, 'author' | 'editor' > & RequireAtLeastOne<
+    StrictSubset< BibTeXFields,
+        'title' | 'booktitle' | 'chapter' | 'pages' | 'publisher' | 'year',
+        'volume' | 'number' | 'series' | 'type' | 'address' | 'edition' | 'month' | 'note'
+    >,
+    'chapter' | 'pages'
+>;
+
+export type IncollectionReference = BaseFields< 'incollection' > & StrictSubset< BibTeXFields,
+    'author' | 'title' | 'booktitle' | 'publisher' | 'year',
+    'editor' | 'volume' | 'number' | 'series' | 'type' | 'chapter' | 'pages' | 'address' | 'edition' | 'month' | 'note'
+>;
+
+export type InproceedingsReference = Conference< 'inproceedings' >;
+
+export type ManualReference = BaseFields< 'manual' > & StrictSubset< BibTeXFields,
+    'title',
+    'author' | 'organization' | 'address' | 'edition' | 'month' | 'year' | 'note'
+>;
+
+export type MastersthesisReference = Thesis< 'mastersthesis' >;
+
+export type ThesisReference = Thesis< 'thesis' >;
+
+export type MiscReference = BaseFields< 'misc' > & ExtractFrom< BibTeXFields,
+    'author' | 'title' | 'howpublished' | 'month' | 'year' | 'note'
+>;
+
+export type PhdthesisReference = Thesis< 'phdthesis' >;
+
+export type ProceedingsReference = BaseFields< 'proceedings' > & StrictSubset< BibTeXFields,
+    'title' | 'year',
+    'editor' | 'volume' | 'number' | 'series' | 'address' | 'month' | 'organization' | 'publisher' | 'note'
+>;
+
+export type TechreportReference = BaseFields< 'techreport' > & StrictSubset< BibTeXFields,
+    'author' | 'title' | 'institution' | 'year',
+    'type' | 'number' | 'address' | 'month' | 'note'
+>;
+
+export type UnpublishedReference = BaseFields< 'unpublished' > & StrictSubset< BibTeXFields,
+    'author' | 'title' | 'note',
+    'month' | 'year'
+>;
