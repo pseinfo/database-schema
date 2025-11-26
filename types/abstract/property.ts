68d0ad54355343fb5b6715d55b5db8d50a86ee75
komed3 <pk97@outlook.de>
2025-11-26 17:32:43 +0100

    add docs

--- types/abstract/property.ts
+++ types/abstract/property.ts
@@ -9,7 +9,15 @@ import { RefId } from './reference';
 import { PhysicalQuantity } from './unit';
 import * as value from './value';
 
-
+/**
+ * BaseFields
+ * Common fields for all property types
+ * 
+ * @template C - Physical quantities used as conditions
+ * @template T - Primitive types for the condition values
+ * @param conditions - optional conditions mapping physical quantities to values
+ * @param references - optional array of reference IDs associated with the property
+ */
 interface BaseFields< C extends PhysicalQuantity = PhysicalQuantity, T extends Primitive = Primitive > {
     conditions?: Conditions< C, T >;
     references?: RefId[];
@@ -17,24 +25,40 @@ interface BaseFields< C extends PhysicalQuantity = PhysicalQuantity, T extends P
 
 /** Specific property type definitions */
 
+/**
+ * PrimitiveProperty
+ * Property type for primitive values
+ */
 export type PrimitiveProperty<
     P extends Primitive = Primitive,
     C extends PhysicalQuantity = PhysicalQuantity,
     T extends Primitive = Primitive
 > = BaseFields< C, T > & value.PrimitiveValue< P >;
 
+/**
+ * SingleProperty
+ * Type describtion of a single value property
+ */
 export type SingleProperty<
     Q extends PhysicalQuantity = PhysicalQuantity,
     C extends PhysicalQuantity = PhysicalQuantity,
     T extends Primitive = Primitive
 > = BaseFields< C, T > & value.SingleValue< Q >;
 
+/**
+ * ArrayProperty
+ * Type describtion of an array value property
+ */
 export type ArrayProperty<
     Q extends PhysicalQuantity = PhysicalQuantity,
     C extends PhysicalQuantity = PhysicalQuantity,
     T extends Primitive = Primitive
 > = BaseFields< C, T > & value.ArrayValue< Q >;
 
+/**
+ * RangeProperty
+ * Type describtion of a range value property
+ */
 export type RangeProperty<
     Q extends PhysicalQuantity = PhysicalQuantity,
     C extends PhysicalQuantity = PhysicalQuantity,
@@ -43,12 +67,20 @@ export type RangeProperty<
 
 /** Coupled property type definitions */
 
+/**
+ * CoupledNumberProperty
+ * Type describtion of a coupled number value property
+ */
 export type CoupledNumberProperty<
     Q extends PhysicalQuantity = PhysicalQuantity,
     C extends PhysicalQuantity = PhysicalQuantity,
     T extends Primitive = Primitive
 > = BaseFields< C, T > & value.CoupledNumberValue< Q >;
 
+/**
+ * CoupledProperty
+ * Type describtion of a coupled value property
+ */
 export type CoupledProperty<
     Q extends PhysicalQuantity = PhysicalQuantity,
     P extends Primitive = Primitive,
@@ -58,12 +90,20 @@ export type CoupledProperty<
 
 /** Union property types */
 
+/**
+ * NumberProperty
+ * Type describtion of a number value property
+ */
 export type NumberProperty<
     Q extends PhysicalQuantity = PhysicalQuantity,
     C extends PhysicalQuantity = PhysicalQuantity,
     T extends Primitive = Primitive
 > = BaseFields< C, T > & value.NumberValue< Q >;
 
+/**
+ * Property
+ * Type describtion of a general property with any value type
+ */
 export type Property<
     Q extends PhysicalQuantity = PhysicalQuantity,
     P extends Primitive = Primitive,
