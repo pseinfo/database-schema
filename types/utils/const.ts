ce26c7676b5024fcb56f7e861a5a40804fc8c191
komed3 <pk97@outlook.de>
2025-11-27 00:23:59 +0100

    Create utils/const.ts

---
+++ types/utils/const.ts
@@ -0,0 +1,48 @@
+/**
+ * Constants
+ * Defines constant values and enumerations used across the database schema.
+ */
+
+/** Phase of matter */
+export const Phase = [ 'solid', 'gaseous', 'liquid', 'plasma', 'unknown' ] as const;
+export type Phase = ( typeof Phase )[ number ];
+
+/** Element blocks */
+export const ElementBlock = [ 's', 'p', 'd', 'f' ] as const;
+export type ElementBlock = ( typeof ElementBlock )[ number ];
+
+/** Shell model */
+export const ShellModel = [ 'k', 'l', 'm', 'n', 'o', 'p', 'q' ] as const;
+export type ShellModel = ( typeof ShellModel )[ number ];
+
+/** Element sets */
+export const ElementSet = [
+    'nonMetal', 'nobleGas', 'alkaliMetal', 'alkalineEarthMetal', 'metalloid', 'halogen',
+    'metal', 'transitionMetal', 'lanthanoide', 'actinoide'
+] as const;
+
+export type ElementSet = ( typeof ElementSet )[ number ];
+
+/** Element groups */
+export const ElementGroup = [
+    'alkaliMetal', 'alkalineEarthMetal', 'scandiumGroup', 'titaniumGroup', 'vanadiumGroup',
+    'chromiumGroup', 'manganeseGroup', 'ironGroup', 'cobaltGroup', 'nickelGroup',
+    'copperGroup', 'zincGroup', 'boronGroup', 'carbonGroup', 'pnictogen', 'chalcogen',
+    'halogen', 'nobleGas', 'lanthanoidSeries', 'actinoidSeries'
+] as const;
+
+export type ElementGroup = ( typeof ElementGroup )[ number ];
+
+/** Element symbols */
+export const ElementSymbol = [
+    'h', 'he', 'li', 'be', 'b', 'c', 'n', 'o', 'f', 'ne', 'na', 'mg', 'al', 'si', 'p', 's',
+    'cl', 'ar', 'k', 'ca', 'sc', 'ti', 'v', 'cr', 'mn', 'fe', 'co', 'ni', 'cu', 'zn', 'ga',
+    'ge', 'as', 'se', 'br', 'kr', 'rb', 'sr', 'y', 'zr', 'nb', 'mo', 'tc', 'ru', 'rh', 'pd',
+    'ag', 'cd', 'in', 'sn', 'sb', 'te', 'i', 'xe', 'cs', 'ba', 'la', 'ce', 'pr', 'nd', 'pm',
+    'sm', 'eu', 'gd', 'tb', 'dy', 'ho', 'er', 'tm', 'yb', 'lu', 'hf', 'ta', 'w', 're', 'os',
+    'ir', 'pt', 'au', 'hg', 'tl', 'pb', 'bi', 'po', 'at', 'rn', 'fr', 'ra', 'ac', 'th', 'pa',
+    'u', 'np', 'pu', 'am', 'cm', 'bk', 'cf', 'es', 'fm', 'md', 'no', 'lr', 'rf', 'db', 'sg',
+    'bh', 'hs', 'mt', 'ds', 'rg', 'cn', 'nh', 'fl', 'mc', 'lv', 'ts', 'og'
+] as const;
+
+export type ElementSymbol = ( typeof ElementSymbol )[ number ];
