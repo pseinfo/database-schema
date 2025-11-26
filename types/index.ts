78a5b40e5296756fc2ecf2b59d4a52d80ff3f31b
komed3 <pk97@outlook.de>
2025-11-26 23:57:17 +0100

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,14 @@
+/**
+ * Database Schema
+ * Defines the root structure of the database schema.
+ */
+
+import { ReferenceCollection } from './abstract/reference';
+import { UnitCollection } from './abstract/unit';
 import { ElementEntity } from './entities/element';
 
 export type Database = {
     elements: ElementEntity;
+    references: ReferenceCollection;
+    units: UnitCollection;
 };
