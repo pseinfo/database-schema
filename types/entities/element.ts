a31bca968506ed650701d19ab53ff0d39dadebf3
komed3 <pk97@outlook.de>
2025-11-27 00:24:18 +0100

    Update element.ts

--- types/entities/element.ts
+++ types/entities/element.ts
@@ -6,20 +6,7 @@
 import { Collection, LangGroup } from '../abstract/collection';
 import { AbundanceGroup, DiscoveryGroup, MediaGroup, MetaData, WeblinksGroup } from '../collections/generic';
 import { RegistryGroup, StructureGroup } from '../collections/registry';
-
-/** List of all element symbols from the periodic table */
-export const ElementSymbol = [
-    'h', 'he', 'li', 'be', 'b', 'c', 'n', 'o', 'f', 'ne', 'na', 'mg', 'al', 'si', 'p', 's',
-    'cl', 'ar', 'k', 'ca', 'sc', 'ti', 'v', 'cr', 'mn', 'fe', 'co', 'ni', 'cu', 'zn', 'ga',
-    'ge', 'as', 'se', 'br', 'kr', 'rb', 'sr', 'y', 'zr', 'nb', 'mo', 'tc', 'ru', 'rh', 'pd',
-    'ag', 'cd', 'in', 'sn', 'sb', 'te', 'i', 'xe', 'cs', 'ba', 'la', 'ce', 'pr', 'nd', 'pm',
-    'sm', 'eu', 'gd', 'tb', 'dy', 'ho', 'er', 'tm', 'yb', 'lu', 'hf', 'ta', 'w', 're', 'os',
-    'ir', 'pt', 'au', 'hg', 'tl', 'pb', 'bi', 'po', 'at', 'rn', 'fr', 'ra', 'ac', 'th', 'pa',
-    'u', 'np', 'pu', 'am', 'cm', 'bk', 'cf', 'es', 'fm', 'md', 'no', 'lr', 'rf', 'db', 'sg',
-    'bh', 'hs', 'mt', 'ds', 'rg', 'cn', 'nh', 'fl', 'mc', 'lv', 'ts', 'og'
-] as const;
-
-export type ElementSymbol = ( typeof ElementSymbol )[ number ];
+import { ElementSymbol } from '../utils/const';
 
 /** Element collections */
 
