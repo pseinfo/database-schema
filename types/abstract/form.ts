ab8450b9ebd4222d920253dd2fb27e7b1f9eec0e
komed3 <pk97@outlook.de>
2025-11-27 09:52:21 +0100

    Update form.ts

--- types/abstract/form.ts
+++ types/abstract/form.ts
@@ -5,18 +5,37 @@
 
 import { Brand } from 'devtypes/types/base';
 import { DeepPartial } from 'devtypes/types/collections';
+import { CrystalStructure, Phase } from '../utils/const';
 import { Collection, Distinct } from './collection';
 
 /** Form types for substances */
 export const FormType = [ 'allotrope', 'molecular', 'phase', 'polymorph', 'amorphous', 'other' ] as const;
 export type FormType = ( typeof FormType )[ number ];
 
-// Field definitions for form types
+/**
+ * BaseFields
+ * Common fields for all form types
+ * 
+ * @template T - Form type
+ * @template C - Collection type for properties
+ * @param properties - Partial collection of properties specific to the form
+ * @param note - Additional notes about the form
+ */
 type BaseFields< T extends FormType, C extends Collection< any > > = Brand< {
     properties?: DeepPartial< C >;
     note?: Distinct< string >;
 }, T >;
 
+/**
+ * FormFields
+ * Specific fields for form properties
+ * 
+ * @param formula - Chemical formula of the form
+ * @param phase - Phase of the substance (solid, liquid, gas, plasma)
+ * @param crystalStructure - Crystal structure type
+ * @param pearsonSymbol - Pearson symbol notation
+ * @param spaceGroup - Space group notation
+ */
 interface FormFields {
     formula?: Distinct< string >;
     phase?: Distinct< Phase >;
