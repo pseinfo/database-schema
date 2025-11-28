26b3285bdf8dbb9541e46f7238f6f6bca0381ad0
komed3 <pk97@outlook.de>
2025-11-28 12:08:06 +0100

    min fix

--- types/abstract/condition.ts
+++ types/abstract/condition.ts
@@ -9,22 +9,22 @@ import { Value } from './value';
 
 /** Definition of special standard conditions */
 export const SpecialCondition = [
-    'STP',          // T=0,     P=100,      IUPAC (STP) since 1982
-    'STP_ATM',      // T=0,     P=101.325,  NIST, ISO 10780, former IUPAC STP
-    'NTP',          // T=20,    P=101.325,  EPA, NIST
-    'ISA',          // T=15,    P=101.325,  ICAO ISA, ISO 13443, EEA, EGIA (SI)
-    'AAPM',         // T=22,    P=101.325,  American Association of Physicists in Medicine
-    'SATP',         // T=25,    P=101.325,  SATP, EPA
-    'CAGI',         // T=20,    P=100,      CAGI
-    'SPE',          // T=15,    P=100,      SPE
-    'ISO_5011',     // T=20,    P=101.3,    ISO 5011
-    'GOST_2939_63', // T=20,    P=101.33,   GOST 2939-63
-    'EGIA',         // T=15.56, P=101.6,    EGIA (Imperial System)
-    'SCF',          // T=15.56, P=101.35,   U.S. DOT (SCF)
-    'AMCA',         // T=21.11, P=101.3,    AMCA
-    'FAA',          // T=15,    P=101.3,    FAA
-    'ISO_13443',    // T=15,    P=101.325,  ISO 2533, ISO 13443, ISO 7504
-    'DIN_1343',     // T=0,     P=101.325,  DIN 1343:1990
+    'STP',          // T=0     P=100      IUPAC (STP) since 1982
+    'STP_ATM',      // T=0     P=101.325  NIST, ISO 10780, former IUPAC STP
+    'NTP',          // T=20    P=101.325  EPA, NIST
+    'ISA',          // T=15    P=101.325  ICAO ISA, ISO 13443, EEA, EGIA (SI)
+    'AAPM',         // T=22    P=101.325  American Association of Physicists in Medicine
+    'SATP',         // T=25    P=101.325  SATP, EPA
+    'CAGI',         // T=20    P=100      CAGI
+    'SPE',          // T=15    P=100      SPE
+    'ISO_5011',     // T=20    P=101.3    ISO 5011
+    'GOST_2939_63', // T=20    P=101.33   GOST 2939-63
+    'EGIA',         // T=15.56 P=101.6    EGIA (Imperial System)
+    'SCF',          // T=15.56 P=101.35   U.S. DOT (SCF)
+    'AMCA',         // T=21.11 P=101.3    AMCA
+    'FAA',          // T=15    P=101.3    FAA
+    'ISO_13443',    // T=15    P=101.325  ISO 2533, ISO 13443, ISO 7504
+    'DIN_1343'      // T=0     P=101.325  DIN 1343:1990
 ] as const;
 
 export type SpecialCondition = ( typeof SpecialCondition )[ number ];
