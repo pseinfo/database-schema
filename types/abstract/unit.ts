e618bcc3ddceed954c81cc273b70c6fedd8af7e9
komed3 <pk97@outlook.de>
2025-11-28 17:26:06 +0100

    define a list of valid quantities and their units

--- types/abstract/unit.ts
+++ types/abstract/unit.ts
@@ -10,3 +10,16 @@ export type MetricSystem = ( typeof MetricSystem )[ number ];
 /** SI base dimensions */
 export const SIDimension = [ 'time', 'length', 'mass', 'electricCurrent', 'temperature', 'amountOfSubstance', 'luminousIntensity' ] as const;
 export type SIDimension = ( typeof SIDimension )[ number ];
+
+/** List of valid quantities and their units */
+export const ValidUnits = {
+    time: [ 's', 'm', 'h', 'd', 'a' ],
+    length: [ 'm', 'in', 'ft', 'yd', 'mi' ],
+    mass: [ 'g', 't', 'oz', 'lb' ],
+    electricCurrent: [ 'A' ],
+    temperature: [ 'K', 'C', 'F' ],
+    amountOfSubstance: [ 'mol' ],
+    luminousIntensity: [ 'cd' ]
+} as const;
+
+export type ValidUnits = typeof ValidUnits;
