652e332ac15b7035c065a89a2f132d2d14798a56
komed3 <pk97@outlook.de>
2025-11-28 17:42:06 +0100

    implement branded unit type

--- types/abstract/unit.ts
+++ types/abstract/unit.ts
@@ -3,6 +3,8 @@
  * Type system for physical units.
  */
 
+import { Brand } from 'devtypes/types/base';
+
 /** Metric systems */
 export const MetricSystem = [ 'metric', 'imperial', 'us', 'custom', 'unknown' ] as const;
 export type MetricSystem = ( typeof MetricSystem )[ number ];
@@ -33,3 +35,29 @@ export type PhysicalQuantity = keyof ValidUnits;
  * [time, length, mass, electricCurrent, temperature, amountOfSubstance, luminousIntensity]
  */
 type DimensionVector = [ number, number, number, number, number, number, number ];
+
+/**
+ * Unit
+ * Type description of a single physical unit.
+ * 
+ * @template Q - physical quantity type
+ * @param symbol - unit symbol (e.g., "m" for meters)
+ * @param name - optional full name of the unit (e.g., "meter")
+ * @param isBase - whether this unit is a base unit
+ * @param allowPrefix - whether unit prefixes are allowed
+ * @param system - metric system the unit belongs to
+ * @param conversion - conversion factor to the base unit
+ *  - factor - multiplication factor to convert to the base unit
+ *  - offset - optional offset for units like Celsius to Kelvin
+ */
+type Unit< Q extends PhysicalQuantity > = Brand< {
+    symbol: string;
+    name?: string;
+    isBase?: boolean;
+    allowPrefix?: boolean;
+    system?: MetricSystem;
+    conversion?: {
+        factor: number;
+        offset?: number;
+    };
+}, `${ Q }::${ ValidUnits[ Q ][ number ] }` >;
