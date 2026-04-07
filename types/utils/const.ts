0e828ed96a8432b23a45da0dbd4ca207b7faa561
komed3 <pk97@outlook.de>
2026-04-08 01:22:11 +0200

    Update utils/const.ts

--- types/utils/const.ts
+++ types/utils/const.ts
@@ -4,18 +4,19 @@
  */
 
 /** Phase of matter */
-export const Phase = [ 'solid', 'gaseous', 'liquid', 'plasma', 'unknown' ] as const;
 export type Phase = ( typeof Phase )[ number ];
+export const Phase = [ 'solid', 'gaseous', 'liquid', 'plasma', 'unknown' ] as const;
 
 /** Natural occurrence types */
-export const NaturalOccurrence = [ 'primordial', 'decayProduct', 'synthetic' ] as const;
 export type NaturalOccurrence = ( typeof NaturalOccurrence )[ number ];
+export const NaturalOccurrence = [ 'primordial', 'decayProduct', 'synthetic' ] as const;
 
 /** Shell model */
-export const ShellModel = [ 'k', 'l', 'm', 'n', 'o', 'p', 'q' ] as const;
 export type ShellModel = ( typeof ShellModel )[ number ];
+export const ShellModel = [ 'k', 'l', 'm', 'n', 'o', 'p', 'q' ] as const;
 
 /** Element symbols */
+export type ElementSymbol = ( typeof ElementSymbol )[ number ];
 export const ElementSymbol = [
     'h', 'he', 'li', 'be', 'b', 'c', 'n', 'o', 'f', 'ne', 'na', 'mg', 'al', 'si', 'p', 's',
     'cl', 'ar', 'k', 'ca', 'sc', 'ti', 'v', 'cr', 'mn', 'fe', 'co', 'ni', 'cu', 'zn', 'ga',
@@ -27,21 +28,19 @@ export const ElementSymbol = [
     'bh', 'hs', 'mt', 'ds', 'rg', 'cn', 'nh', 'fl', 'mc', 'lv', 'ts', 'og'
 ] as const;
 
-export type ElementSymbol = ( typeof ElementSymbol )[ number ];
-
 /** Element blocks */
-export const ElementBlock = [ 's', 'p', 'd', 'f' ] as const;
 export type ElementBlock = ( typeof ElementBlock )[ number ];
+export const ElementBlock = [ 's', 'p', 'd', 'f' ] as const;
 
 /** Element sets */
+export type ElementSet = ( typeof ElementSet )[ number ];
 export const ElementSet = [
     'nonMetal', 'nobleGas', 'alkaliMetal', 'alkalineEarthMetal', 'metalloid', 'halogen',
     'metal', 'transitionMetal', 'lanthanoide', 'actinoide'
 ] as const;
 
-export type ElementSet = ( typeof ElementSet )[ number ];
-
 /** Element groups */
+export type ElementGroup = ( typeof ElementGroup )[ number ];
 export const ElementGroup = [
     'alkaliMetal', 'alkalineEarthMetal', 'scandiumGroup', 'titaniumGroup', 'vanadiumGroup',
     'chromiumGroup', 'manganeseGroup', 'ironGroup', 'cobaltGroup', 'nickelGroup',
@@ -49,22 +48,18 @@ export const ElementGroup = [
     'halogen', 'nobleGas', 'lanthanoidSeries', 'actinoidSeries'
 ] as const;
 
-export type ElementGroup = ( typeof ElementGroup )[ number ];
-
 /** Element properties */
+export type ElementProperty = ( typeof ElementProperty )[ number ];
 export const ElementProperty = [
     'antiquity', 'artificial', 'heavyMetal', 'lightMetal', 'mononuclide', 'native',
     'natural', 'noble', 'platinumMetal', 'radioactive', 'rareEarths', 'refractorMetal',
     'semiconductor', 'stable', 'synthetic', 'vital'
 ] as const;
 
-export type ElementProperty = ( typeof ElementProperty )[ number ];
-
 /** Crystal structure */
+export type CrystalStructure = ( typeof CrystalStructure )[ number ];
 export const CrystalStructure = [
     'hexagonal', 'hexagonalClosePacked', 'bodyCenteredCubic', 'rhombohedral',
     'simpleCubic', 'faceCenteredCubic', 'diamondCubic', 'orthorhombic', 'tetragonal',
     'doubleHexagonalClosePacked', 'monoclinic', 'triclinic'
 ] as const;
-
-export type CrystalStructure = ( typeof CrystalStructure )[ number ];
