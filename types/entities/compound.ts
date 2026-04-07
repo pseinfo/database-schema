c8cce2b1131b61b1b409ce7a0373714078de786a
komed3 <pk97@outlook.de>
2026-04-08 01:03:01 +0200

    define compound component roles

--- types/entities/compound.ts
+++ types/entities/compound.ts
@@ -11,8 +11,15 @@ import { SafetyCollection } from '../collections/safety';
 import { RegistryGroup, StructureGroup } from '../collections/registry';
 import { DiscoveryGroup, MediaGroup, MetaData, WeblinksGroup } from '../collections/generic';
 import { ElementSymbol } from '../utils/const';
+import { LiteralUnion } from 'devtypes/types/primitive';
 
 /** Compound collections */
+
+/**
+ * Component Role
+ * Defines the possible roles of components within a chemical compound.
+ */
+export type CompoundComponentRole = LiteralUnion< 'cation' | 'anion' | 'neutral' | 'radical' | 'complex' >;
 
 /**
  * Component Component
@@ -20,16 +27,16 @@ import { ElementSymbol } from '../utils/const';
  * 
  * @param element - Chemical symbol of the element in the component
  * @param amount - Amount of the element in the compound (e.g., moles, mass)
+ * @param role - Optional role of the component in the compound (e.g., cation, anion)
  * @param massFraction - Optional mass fraction of the element in the compound
  * @param atomicFraction - Optional atomic fraction of the element in the compound
- * @param role - Optional role of the component in the compound (e.g., cation, anion)
  */
 type CompoundComponent = Group< {
     element: ElementSymbol;
     amount: Distinct< number >;
+    role?: Distinct< CompoundComponentRole >;
     massFraction?: Distinct< number >;
     atomicFraction?: Distinct< number >;
-    role?: string;
 } >;
 
 /** Main compound entity */
