973873e75c63dc8f16cd8358869640c20da6f76e
komed3 <pk97@outlook.de>
2026-04-12 09:45:28 +0200

    Update composition.ts

--- types/collections/composition.ts
+++ types/collections/composition.ts
@@ -3,14 +3,15 @@
  * Defines the structure for chemical components and composition of substances.
  */
 
-import type { Collection, Distinct } from '../abstract/collection';
-import type { ElementSymbol, ComponentRole } from '../utils/const';
+import type { Collection, Distinct, Group, Single } from '@/abstract/collection';
+import type { PrimitiveProperty } from '@/abstract/property';
+import type { ElementSymbol, ComponentRole } from '@/utils/const';
 
 /**
  * CompositionComponent
  * Defines the structure for a single component of a chemical substance or mineral.
  * 
- * @param element - Chemical symbol of the element
+ * @param element - Distinct chemical symbol of the element
  * @param amount - Stoichiometric amount or number of atoms
  * @param frequency - Optional occurrence frequency or probability
  * @param role - Role of the component (e.g., cation, anion, impurity)
@@ -18,14 +19,14 @@ import type { ElementSymbol, ComponentRole } from '../utils/const';
  * @param atomicFraction - Optional atomic fraction of the element
  * @param charge - Optional formal charge of the component
  */
-export type CompositionComponent = Distinct< {
-    element: ElementSymbol;
-    amount: Distinct< number >;
-    frequency?: Distinct< number >;
-    role?: Distinct< ComponentRole >;
-    massFraction?: Distinct< number >;
-    atomicFraction?: Distinct< number >;
-    charge?: Distinct< number >;
+export type CompositionComponent = Group< {
+    element: Distinct< ElementSymbol >;
+    amount: Single< PrimitiveProperty< number > >;
+    frequency?: Single< PrimitiveProperty< number > >;
+    role?: Single< PrimitiveProperty< ComponentRole > >;
+    massFraction?: Single< PrimitiveProperty< number > >;
+    atomicFraction?: Single< PrimitiveProperty< number > >;
+    charge?: Single< PrimitiveProperty< number > >;
 } >;
 
 /**
@@ -39,9 +40,9 @@ export type CompositionComponent = Distinct< {
  * @param charge - Overall formal charge
  */
 export type CompositionCollection = Collection< {
-    components: Distinct< CompositionComponent[] >;
-    formula: Distinct< string >;
-    empiricalFormula?: Distinct< string >;
-    structuralFormula?: Distinct< string >;
-    charge?: Distinct< number >;
+    components: CompositionComponent[];
+    formula: Single< PrimitiveProperty< string > >;
+    empiricalFormula?: Single< PrimitiveProperty< string > >;
+    structuralFormula?: Single< PrimitiveProperty< string > >;
+    charge?: Single< PrimitiveProperty< number > >;
 } >;
