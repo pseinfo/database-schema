5f499834b89472c6ba50598c99489b6b91bc31bc
komed3 <pk97@outlook.de>
2026-04-13 22:32:41 +0200

    add refs / units to db

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,9 @@
+import type { ReferenceCollection } from './abstract/reference';
+import type { UnitCollection } from './abstract/unit';
+
 export type Database = {
   meta: any;
   data: any;
-  unit: any;
-  refs: any;
+  unit: UnitCollection;
+  refs: ReferenceCollection;
 };
