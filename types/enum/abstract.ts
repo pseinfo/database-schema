0cc5c91ccb63a5a8a0b261b46a2b372b4ace69e1
komed3 <pk97@outlook.de>
2026-04-13 20:03:59 +0200

    declare abstract enums for unit system

---
+++ types/enum/abstract.ts
@@ -0,0 +1,17 @@
+export enum MetricSystem {
+    METRIC = 'metric',
+    IMPERIAL = 'imperial',
+    US = 'us',
+    CUSTOM = 'custom',
+    UNKNOWN = 'unknown'
+}
+
+export enum SIDimension {
+    TIME = 'time',
+    LENGTH = 'length',
+    MASS = 'mass',
+    ELECTRIC_CURRENT = 'electricCurrent',
+    TEMPERATURE = 'temperature',
+    AMOUNT_OF_SUBSTANCE = 'amountOfSubstance',
+    LUMINOUS_INTENSITY = 'luminousIntensity'
+}
