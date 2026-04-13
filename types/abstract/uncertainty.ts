fb7c76a8aba80f68c251ab13e63f951066dbbe2b
komed3 <pk97@outlook.de>
2026-04-13 22:34:36 +0200

    min fixes

--- types/abstract/uncertainty.ts
+++ types/abstract/uncertainty.ts
@@ -3,33 +3,33 @@ import type { Brand, Expand } from 'devtypes/types/util';
 import type { UncertaintyType } from '../enum/abstract';
 
 export type BaseUncertainty< T extends UncertaintyType > = Brand< {
-    confidence?: number;
-    note?: string;
+  confidence?: number;
+  note?: string;
 }, T, 'type', true >;
 
 export interface UncertaintyFields {
-    absolute?: number;
-    relative?: number;
-    plus?: number;
-    minus?: number;
+  absolute?: number;
+  relative?: number;
+  plus?: number;
+  minus?: number;
 }
 
 export type AbsoluteUncertainty = Expand<
-    BaseUncertainty< UncertaintyType.ABSOLUTE > &
-    RequireFrom< UncertaintyFields, 'absolute' >
+  BaseUncertainty< UncertaintyType.ABSOLUTE > &
+  RequireFrom< UncertaintyFields, 'absolute' >
 >;
 
 export type RelativeUncertainty = Expand<
-    BaseUncertainty< UncertaintyType.RELATIVE > &
-    RequireFrom< UncertaintyFields, 'relative' >
+  BaseUncertainty< UncertaintyType.RELATIVE > &
+  RequireFrom< UncertaintyFields, 'relative' >
 >;
 
 export type AsymmetricalUncertainty = Expand<
-    BaseUncertainty< UncertaintyType.ASYMMETRICAL > &
-    RequireFrom< UncertaintyFields, 'plus' | 'minus' >
+  BaseUncertainty< UncertaintyType.ASYMMETRICAL > &
+  RequireFrom< UncertaintyFields, 'plus' | 'minus' >
 >;
 
 export type Uncertainty =
-    | AbsoluteUncertainty
-    | RelativeUncertainty
-    | AsymmetricalUncertainty;
+  | AbsoluteUncertainty
+  | RelativeUncertainty
+  | AsymmetricalUncertainty;
--- types/enum/abstract.ts
+++ types/enum/abstract.ts
@@ -23,19 +23,19 @@ export enum UncertaintyType {
 };
 
 export enum ReferenceType {
-    ARTICLE = 'article',
-    BOOK = 'book',
-    BOOKLET = 'booklet',
-    CONFERENCE = 'conference',
-    INBOOK = 'inbook',
-    INCOLLECTION = 'incollection',
-    INPROCEEDINGS = 'inproceedings',
-    MANUAL = 'manual',
-    MASTERSTHESIS = 'mastersthesis',
-    THESIS = 'thesis',
-    MISC = 'misc',
-    PHDTHESIS = 'phdthesis',
-    PROCEEDINGS = 'proceedings',
-    TECHREPORT = 'techreport',
-    UNPUBLISHED = 'unpublished'
+  ARTICLE = 'article',
+  BOOK = 'book',
+  BOOKLET = 'booklet',
+  CONFERENCE = 'conference',
+  INBOOK = 'inbook',
+  INCOLLECTION = 'incollection',
+  INPROCEEDINGS = 'inproceedings',
+  MANUAL = 'manual',
+  MASTERSTHESIS = 'mastersthesis',
+  THESIS = 'thesis',
+  MISC = 'misc',
+  PHDTHESIS = 'phdthesis',
+  PROCEEDINGS = 'proceedings',
+  TECHREPORT = 'techreport',
+  UNPUBLISHED = 'unpublished'
 };
