6fdaf9708aa4d983e9d71425a7ee08cf0ad1eab5
komed3 <pk97@outlook.de>
2026-04-13 15:20:36 +0200

    start with basics

--- types/collections/composition.ts
+++ types/collections/composition.ts
@@ -5,9 +5,7 @@
  * @module collections/composition
  */
 
-import type { Collection, Distinct, Group, Single } from '@/abstract/collection';
-import type { NumberProperty, PrimitiveProperty } from '@/abstract/property';
-import type { ComponentRole } from '@/enums/compound';
+import type { Collection, Distinct, Group } from '@/abstract/collection';
 import type { ElementSymbol } from '@/enums/element';
 
 
@@ -16,35 +14,19 @@ import type { ElementSymbol } from '@/enums/element';
  * 
  * @param element - Distinct chemical symbol of the element
  * @param amount - Distinct stoichiometric amount or number of atoms
- * @param frequency - Optional occurrence frequency or probability
- * @param role - Role of the component (e.g., cation, anion, impurity)
- * @param massFraction - Optional mass fraction of the element
- * @param atomicFraction - Optional atomic fraction of the element
- * @param charge - Optional formal charge of the component
  */
 export type CompositionComponent = Group< {
     element: Distinct< ElementSymbol >;
     amount: Distinct< number >;
-    frequency?: Single< PrimitiveProperty< number > >;
-    role?: Single< PrimitiveProperty< ComponentRole > >;
-    massFraction?: Single< NumberProperty< 'massFraction' > >;
-    atomicFraction?: Single< NumberProperty< 'massFraction' > >;
-    charge?: Single< PrimitiveProperty< number > >;
 } >;
 
 /**
  * Collection for compositional properties.
  * 
+ * @param formula - Chemical formula
  * @param components - List of components that make up the substance
- * @param formula - Distinct chemical formula
- * @param empiricalFormula - Distinct empirical formula
- * @param structuralFormula - Distinct structural formula (e.g. for minerals or complex compounds)
- * @param charge - Overall formal charge
  */
 export type CompositionCollection = Collection< {
-    components: CompositionComponent[];
     formula: Distinct< string >;
-    empiricalFormula?: Distinct< string >;
-    structuralFormula?: Distinct< string >;
-    charge?: Single< PrimitiveProperty< number > >;
+    components: CompositionComponent[];
 } >;
