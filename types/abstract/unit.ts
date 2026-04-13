7d0fb20d0ac1fef6285f1a59e523e55426b6493c
komed3 <pk97@outlook.de>
2026-04-13 20:30:31 +0200

    define physical quantity type

--- types/abstract/unit.ts
+++ types/abstract/unit.ts
@@ -4,9 +4,9 @@ import type { MetricSystem, SIDimension } from '../enum/abstract';
 
 export type SIPrefix = keyof typeof SIPrefix;
 export const SIPrefix = {
-    Y: 1e24, Z: 1e21, E: 1e18, P: 1e15, T: 1e12, G: 1e9, M: 1e6, k: 1e3,
-    h: 1e2, da: 1e1, d: 1e-1, c: 1e-2, m: 1e-3, µ: 1e-6, n: 1e-9, p: 1e-12,
-    f: 1e-15, a: 1e-18, z: 1e-21, y: 1e-24
+  Y: 1e24, Z: 1e21, E: 1e18, P: 1e15, T: 1e12, G: 1e9, M: 1e6, k: 1e3,
+  h: 1e2, da: 1e1, d: 1e-1, c: 1e-2, m: 1e-3, µ: 1e-6, n: 1e-9, p: 1e-12,
+  f: 1e-15, a: 1e-18, z: 1e-21, y: 1e-24
 } as const;
 
 export type Units = typeof units;
@@ -16,18 +16,31 @@ export type BaseUnitSymbols< Q extends PhysicalQuantity > = Units[ Q ][ 'base' ]
 export type PrefixableUnitSymbols< Q extends PhysicalQuantity > = Units[ Q ][ 'prefixable' ][ number ];
 
 export type PrefixedSymbols< Q extends PhysicalQuantity > =
-    | BaseUnitSymbols< Q >
-    | `${ SIPrefix }${ PrefixableUnitSymbols< Q > }`;
+  | BaseUnitSymbols< Q >
+  | `${ SIPrefix }${ PrefixableUnitSymbols< Q > }`;
 
 export type DimensionVector = [ number, number, number, number, number, number, number ];
 
 export type Unit< Q extends PhysicalQuantity, U extends BaseUnitSymbols< Q > > = Brand< {
-    name?: string;
-    isBase?: boolean;
-    prefixable?: U extends PrefixableUnitSymbols< Q > ? true : false;
-    system?: MetricSystem;
-    conversion?: {
-        factor: number;
-        offset?: number;
-    };
+  name?: string;
+  isBase?: boolean;
+  prefixable?: U extends PrefixableUnitSymbols< Q > ? true : false;
+  system?: MetricSystem;
+  conversion?: {
+    factor: number;
+    offset?: number;
+  };
 }, U, 'symbol', true >;
+
+export type Quantity< Q extends PhysicalQuantity > = {
+  dimension?: {
+    symbol: string;
+    name: string;
+    si: Q extends SIDimension ? true : false;
+    vector: DimensionVector;
+  };
+  baseUnit: BaseUnitSymbols< Q >;
+  units: {
+    [ U in BaseUnitSymbols< Q > ]: Unit< Q, U >;
+  };
+};
--- types/enum/abstract.ts
+++ types/enum/abstract.ts
@@ -1,17 +1,17 @@
 export enum MetricSystem {
-    METRIC = 'metric',
-    IMPERIAL = 'imperial',
-    US = 'us',
-    CUSTOM = 'custom',
-    UNKNOWN = 'unknown'
-}
+  METRIC = 'metric',
+  IMPERIAL = 'imperial',
+  US = 'us',
+  CUSTOM = 'custom',
+  UNKNOWN = 'unknown'
+};
 
 export enum SIDimension {
-    TIME = 'time',
-    LENGTH = 'length',
-    MASS = 'mass',
-    ELECTRIC_CURRENT = 'electricCurrent',
-    TEMPERATURE = 'temperature',
-    AMOUNT_OF_SUBSTANCE = 'amountOfSubstance',
-    LUMINOUS_INTENSITY = 'luminousIntensity'
-}
+  TIME = 'time',
+  LENGTH = 'length',
+  MASS = 'mass',
+  ELECTRIC_CURRENT = 'electricCurrent',
+  TEMPERATURE = 'temperature',
+  AMOUNT_OF_SUBSTANCE = 'amountOfSubstance',
+  LUMINOUS_INTENSITY = 'luminousIntensity'
+};
