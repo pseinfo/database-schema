c0cdc7f489c49af6591c1842a831d1b41697e810
komed3 <pk97@outlook.de>
2026-04-14 01:11:16 +0200

    Create condition.ts

---
+++ types/abstract/condition.ts
@@ -0,0 +1,13 @@
+import type { Primitive } from 'devtypes/types/primitive';
+import { stp } from '../../config/stp';
+import type { PhysicalQuantity } from './unit';
+import type { Value } from './value';
+
+export type StandardCondition = keyof typeof stp;
+
+export type Conditions<
+    Q extends PhysicalQuantity = PhysicalQuantity,
+    T extends Primitive = Primitive
+> = StandardCondition | {
+    [ K in Q ]?: Value< K, T >;
+};
