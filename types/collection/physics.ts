ef75ca626489b62f6c1424f6a1352b4df6b196b5
komed3 <pk97@outlook.de>
2026-04-14 12:56:39 +0200

    add hardness / elasticity / electricity props.

--- types/collection/physics.ts
+++ types/collection/physics.ts
@@ -1,6 +1,7 @@
 import type { Collection, Group, Single } from '../abstract/collection';
 import type { CoupledNumberProperty, NumberProperty, PrimitiveProperty } from '../abstract/property';
 import type { Phase } from '../enum/generic';
+import type { Superconductivity } from '../enum/physics';
 
 export type PhysicsCollection = Collection< {
   density?: Single< NumberProperty< 'density' > >;
@@ -37,5 +38,34 @@ export type PhysicsCollection = Collection< {
     thermalDiffusivity?: Single< NumberProperty< 'thermalDiffusivity' > >;
     workFunction?: Single< NumberProperty< 'energy' > >;
     adiabaticIndex?: Single< PrimitiveProperty< number > >;
+  } >;
+
+  hardness?: Group< {
+    brinellHardness?: Single< PrimitiveProperty< number > >;
+    mohsHardness?: Single< PrimitiveProperty< number > >;
+    vickersHardness?: Single< PrimitiveProperty< number > >;
+    rockwellHardness?: Single< PrimitiveProperty< number > >;
+    knoopHardness?: Single< PrimitiveProperty< number > >;
+  } >;
+
+  elasticity?: Group< {
+    bulkModulus?: Single< NumberProperty< 'pressure' > >;
+    shearModulus?: Single< NumberProperty< 'pressure' > >;
+    youngModulus?: Single< NumberProperty< 'pressure' > >;
+    poissonRatio?: Single< PrimitiveProperty< number > >;
+    compressibility?: Single< NumberProperty< 'compressibility' > >;
+    tensileStrength?: Single< NumberProperty< 'pressure' > >;
+    yieldStrength?: Single< NumberProperty< 'pressure' > >;
+    ultimateStrength?: Single< NumberProperty< 'pressure' > >;
+  } >;
+
+  electricity?: Group< {
+    conductivity?: Single< NumberProperty< 'electricConductivity' > >;
+    resistivity?: Single< NumberProperty< 'electricResistivity' > >;
+    temperatureCoefficient?: Single< NumberProperty< 'tempCoefficient' > >;
+    superconductivity?: Single< PrimitiveProperty< Superconductivity > >;
+    superconductingPoint?: Single< NumberProperty< 'temperature' > >;
+    bandGap?: Single< NumberProperty< 'energy' > >;
+    dielectricConstant?: Single< PrimitiveProperty< number > >;
   } >;
 } >;
---
+++ types/enum/physics.ts
@@ -0,0 +1,5 @@
+export enum Superconductivity {
+  NORMAL = 'normal',
+  SPECIAL = 'special',
+  NONE = 'none'
+};
