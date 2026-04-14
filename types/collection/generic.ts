4a3c39f125fae4cd8eb91b36b4750a477677a9af
komed3 <pk97@outlook.de>
2026-04-14 09:54:45 +0200

    declare media / weblink groups

--- types/collection/generic.ts
+++ types/collection/generic.ts
@@ -1,6 +1,8 @@
 import type { Expand } from 'devtypes/types/util';
-import type { Distinct, Group } from '../abstract/collection';
+import type { Distinct, Group, Single } from '../abstract/collection';
 import type { LangCode } from '../enum/generic';
+import { D3Format, ImageFormat } from '../enum/util';
+import { PrimitiveProperty } from '../abstract/property';
 
 export type MetaData = Distinct< {
   '@metadata': {
@@ -13,3 +15,41 @@ export type LangGroup< L extends LangCode = LangCode.ENGLISH, T = string > = Gro
   Required< { [ K in L ]: Distinct< T > } > &
   Partial< { [ K in Exclude< LangCode, L > ]: Distinct< T > } >
 > >;
+
+export type MediaGroup = Group< {
+  images?: Distinct< {
+    url: string;
+    format?: ImageFormat;
+    credits: string;
+    license: string;
+    author?: string;
+    source?: string;
+    width?: number;
+    height?: number;
+  }[] >;
+
+  spectrum?: Group< {
+    absorption?: Single< PrimitiveProperty< string > >;
+    emission?: Single< PrimitiveProperty< string > >;
+    uv?: Single< PrimitiveProperty< string > >;
+    xray?: Single< PrimitiveProperty< string > >;
+  } >;
+
+  structure3D?: Distinct< {
+    data: string;
+    format: D3Format;
+    url?: string;
+  }[] >;
+} >;
+
+export type WeblinksGroup = Group< {
+  links?: Distinct< {
+    url: string;
+    text?: string;
+    description?: string;
+    archiveUrl?: string;
+    accessed?: string;
+    language?: string;
+  }[] >;
+  wiki?: LangGroup;
+} >;
