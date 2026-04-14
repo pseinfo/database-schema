d094b8dd636f4ff10aeeb4023141f30a65ea26b4
komed3 <pk97@outlook.de>
2026-04-14 12:12:19 +0200

    define chemistry enums

---
+++ types/enum/chemistry.ts
@@ -0,0 +1,31 @@
+export enum AcidBaseCharacter {
+  NEUTRAL = 'neutral',
+  STRONG_ACIDIC = 'strongAcidic',
+  ACIDIC = 'acidic',
+  MODERATE_ACIDIC = 'moderateAcidic',
+  WEAK_ACIDIC = 'weakAcidic',
+  AMPHOTERIC = 'amphoteric',
+  WEAK_BASIC = 'weakBasic',
+  MODERATE_BASIC = 'moderateBasic',
+  BASIC = 'basic',
+  STRONG_BASIC = 'strongBasic'
+};
+
+export enum BasicityType {
+  ACID = 'acid',
+  BASE = 'base'
+};
+
+export enum HSAB {
+  HARD = 'hard',
+  EDGE = 'edge',
+  SOFT = 'soft'
+};
+
+export enum Goldschmidt {
+  ATMOPHILE = 'atmophile',
+  CHALCOPHILE = 'chalcophile',
+  LITHOPHILE = 'lithophile',
+  SIDEROPHILE = 'siderophile',
+  SYNTHETIC = 'synthetic'
+};
