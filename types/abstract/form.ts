22210f70135fc38e65e981baa1c86c835c98a713
komed3 <pk97@outlook.de>
2026-04-14 17:59:35 +0200

    Create form.ts

---
+++ types/abstract/form.ts
@@ -0,0 +1,26 @@
+import type { RequireFrom } from 'devtypes/types/constraint';
+import type { DeepPartial } from 'devtypes/types/transform';
+import type { Brand, Expand } from 'devtypes/types/util';
+import type { Phase } from '../enum/generic';
+import type { FormType } from '../enum/util';
+import type { Collection, Distinct } from './collection';
+
+export type BaseForm< T extends FormType, C extends Collection< any > > = Brand< {
+  properties?: DeepPartial< C >;
+  note?: Distinct< string >;
+}, T, 'type', true >;
+
+export interface FormFields {
+  formula?: Distinct< string >;
+  phase?: Distinct< Phase >;
+};
+
+export type MolecularForm< C extends Collection< any > > = Expand<
+  BaseForm< FormType.MOLECULAR, C > &
+  RequireFrom< FormFields, 'formula' >
+>;
+
+export type PhaseForm< C extends Collection< any > > = Expand<
+  BaseForm< FormType.PHASE, C > &
+  RequireFrom< FormFields, 'phase' >
+>;
