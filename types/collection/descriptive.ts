237fd224a538e827ac73fa525e987f632c6a9770
komed3 <pk97@outlook.de>
2026-04-14 09:49:45 +0200

    add naming and descriptive types

--- types/collection/descriptive.ts
+++ types/collection/descriptive.ts
@@ -1,9 +1,13 @@
-import type { Collection, Single } from '../abstract/collection';
-import type { NumberProperty } from '../abstract/property';
+import type { Collection } from '../abstract/collection';
 import type { LangCode } from '../enum/generic';
+import type { LangGroup } from './generic';
 import type { RegistryGroup, StructureGroup } from './registry';
 
 export type DescriptiveCollection = Collection< {
   registry: RegistryGroup;
   structure: StructureGroup;
+  names: LangGroup< LangCode.ENGLISH | LangCode.LATIN >;
+  description?: LangGroup;
+  appearance?: LangGroup;
+  odor?: LangGroup;
 } >;
