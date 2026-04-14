bb37bf0c4428e871bf8980215ad86d8fb74e9adc
komed3 <pk97@outlook.de>
2026-04-14 10:23:00 +0200

    add element specific enums

---
+++ types/enum/element.ts
@@ -0,0 +1,56 @@
+export enum ElementBlock { S = 's', P = 'p', D = 'd', F = 'f' };
+
+export enum ElementSet {
+  NON_METAL = 'nonMetal',
+  NOBLE_GAS = 'nobleGas',
+  ALKALI_METAL = 'alkaliMetal',
+  ALKALINE_EARTH_METAL = 'alkalineEarthMetal',
+  METALLOID = 'metalloid',
+  HALOGEN = 'halogen',
+  METAL = 'metal',
+  TRANSITION_METAL = 'transitionMetal',
+  LANTHANOIDE = 'lanthanoid',
+  ACTINOIDE = 'actinoide'
+};
+
+export enum ElementGroup {
+  ALKALI_METAL = 'alkaliMetal',
+  ALKALINE_EARTH_METAL = 'alkalineEarthMetal',
+  SCANDIUM_GROUP = 'scandiumGroup',
+  TITANIUM_GROUP = 'titaniumGroup',
+  VANADIUM_GROUP = 'vanadiumGroup',
+  CHROMIUM_GROUP = 'chromiumGroup',
+  MANGANESE_GROUP = 'manganeseGroup',
+  IRON_GROUP = 'ironGroup',
+  COBALT_GROUP = 'cobaltGroup',
+  NICKEL_GROUP = 'nickelGroup',
+  COPPER_GROUP = 'copperGroup',
+  ZINC_GROUP = 'zincGroup',
+  BORON_GROUP = 'boronGroup',
+  CARBON_GROUP = 'carbonGroup',
+  PNICTOGEN = 'pnictogen',
+  CHALCOGEN = 'chalcogen',
+  HALOGEN = 'halogen',
+  NOBLE_GAS = 'nobleGas',
+  LANTHANOID_SERIES = 'lanthanoidSeries',
+  ACTINOID_SERIES = 'actinoidSeries'
+};
+
+export enum ElementProperty {
+  ANTIQUITY = 'antiquity',
+  ARTIFICIAL = 'artificial',
+  HEAVY_METAL = 'heavyMetal',
+  LIGHT_METAL = 'lightMetal',
+  MONONUCLEIDE = 'mononuclide',
+  NATIVE = 'native',
+  NATURAL = 'natural',
+  NOBLE = 'noble',
+  PLATINUM_METAL = 'platinumMetal',
+  RADIOACTIVE = 'radioactive',
+  RARE_EARTHS = 'rareEarths',
+  REFRACTOR_METAL = 'refractorMetal',
+  SEMICONDUCTOR = 'semiconductor',
+  STABLE = 'stable',
+  SYNTHETIC = 'synthetic',
+  VITAL = 'vital'
+};
