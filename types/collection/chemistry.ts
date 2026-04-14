9495d42c4703c944bdd44b2baec0f278ed1dd916
komed3 <pk97@outlook.de>
2026-04-14 12:12:41 +0200

    add basicity props.

--- types/collection/chemistry.ts
+++ types/collection/chemistry.ts
@@ -1,7 +1,17 @@
-import type { Collection, Single } from '../abstract/collection';
-import type { NumberProperty } from '../abstract/property';
+import type { Collection, Group, Single } from '../abstract/collection';
+import type { NumberProperty, PrimitiveProperty } from '../abstract/property';
+import type { AcidBaseCharacter, BasicityType, Goldschmidt, HSAB } from '../enum/chemistry';
 
 export type ChemistryCollection = Collection< {
-    molarMass?: Single< NumberProperty< 'molarMass' > >;
-    molarVolume?: Single< NumberProperty< 'molarVolume' > >;
+  molarMass?: Single< NumberProperty< 'molarMass' > >;
+  molarVolume?: Single< NumberProperty< 'molarVolume' > >;
+
+  basicity?: Group< {
+    type?: Single< PrimitiveProperty< BasicityType > >;
+    hsab?: Single< PrimitiveProperty< HSAB > >;
+    character?: Single< PrimitiveProperty< AcidBaseCharacter > >;
+    pKa?: Single< PrimitiveProperty< number > >;
+    pKb?: Single< PrimitiveProperty< number > >;
+    goldschmidt?: Single< PrimitiveProperty< Goldschmidt > >;
+  } >;
 } >;
