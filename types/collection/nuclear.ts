c67d1e46abe343636b308ca5a6b4db4fe7f7bcfd
komed3 <pk97@outlook.de>
2026-04-14 20:00:27 +0200

    describe nuclear properties

--- types/collection/nuclear.ts
+++ types/collection/nuclear.ts
@@ -1,7 +1,31 @@
-import type { Collection, Single } from '../abstract/collection';
-import type { NumberProperty } from '../abstract/property';
+import type { Collection, Group, Single } from '../abstract/collection';
+import type { NumberProperty, PrimitiveProperty, StructProperty } from '../abstract/property';
+import type { SpinParity } from '../enum/nuclide';
 
 export type NuclearCollection = Collection< {
-  atomicMass?: Single< NumberProperty< 'mass' > >;
+  halfLife?: Single< NumberProperty< 'time' > >;
+  isotopeMass?: Single< NumberProperty< 'mass' > >;
+  chargeRadius?: Single< NumberProperty< 'length' > >;
   massExcess?: Single< NumberProperty< 'energy' > >;
+
+  dipoleMoment?: Single< NumberProperty< 'area' > >;
+  quadrupoleMoment?: Single< NumberProperty< 'magneticMoment' > >;
+
+  isoSpin?: Single< PrimitiveProperty< number > >;
+  spin?: Single< StructProperty < {
+    value: string;
+    parity: SpinParity;
+  } > >;
+
+  energy?: Group< {
+    bindingEnergy?: Single< NumberProperty< 'energy' > >;
+    alphaDecayEnergy?: Single< NumberProperty< 'energy' > >;
+    betaDecayEnergy?: Single< NumberProperty< 'energy' > >;
+    electronCaptureEnergy?: Single< NumberProperty< 'energy' > >;
+    neutronEmissionEnergy?: Single< NumberProperty< 'energy' > >;
+    isomericTransitionEnergy?: Single< NumberProperty< 'energy' > >;
+    neutronSeparationEnergy?: Single< NumberProperty< 'energy' > >;
+    protonSeparationEnergy?: Single< NumberProperty< 'energy' > >;
+    excitationEnergy?: Single< NumberProperty< 'energy' > >;
+  } >;
 } >;
