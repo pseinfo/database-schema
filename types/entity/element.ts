d88610b170e5f8641838eeca760d72dbf29154b8
komed3 <pk97@outlook.de>
2026-04-14 10:12:57 +0200

    describe element collection

--- types/entity/element.ts
+++ types/entity/element.ts
@@ -1,1 +1,15 @@
-export type Element = {}
+import type { Expand } from 'devtypes/types/util';
+import type { Collection } from '../abstract/collection';
+import type { DescriptiveCollection } from '../collection/descriptive';
+import type { MetaData } from '../collection/generic';
+import type { ElementSymbol } from '../enum/generic';
+
+export type SingleElement = {
+  descriptive: DescriptiveCollection;
+};
+
+export type Element = Expand< MetaData & SingleElement >;
+
+export type ElementCollection = Collection< {
+  [ K in ElementSymbol ]: Element;
+} >;
--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,12 @@
 import type { Collection } from './abstract/collection';
 import type { ReferenceCollection } from './abstract/reference';
 import type { UnitCollection } from './abstract/unit';
+import type { ElementCollection } from './entity/element';
 
 export type Database = {
   meta: any;
   data: Collection< {
-    elements: any;
+    elements: ElementCollection;
     nuclides: any;
     compounds: any;
     minerals: any;
