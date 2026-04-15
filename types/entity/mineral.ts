51d2dbdf67353b7987172eaaedd5b7d3352866ff
komed3 <pk97@outlook.de>
2026-04-15 15:39:35 +0200

    mineral classes

--- types/entity/mineral.ts
+++ types/entity/mineral.ts
@@ -1,18 +1,32 @@
 import type { Brand, Expand } from 'devtypes/types/util';
-import type { Collection } from '../abstract/collection';
+import type { Collection, Distinct } from '../abstract/collection';
 import type { FormCollection } from '../abstract/form';
 import type { MetaData } from '../abstract/util';
 import type { AbundanceCollection } from '../collection/abundance';
 import type { ChemistryCollection } from '../collection/chemistry';
+import type { CompositionCollection } from '../collection/composition';
 import type { DescriptiveCollection } from '../collection/descriptive';
 import type { GenericCollection } from '../collection/generic';
 import type { PhysicsCollection } from '../collection/physics';
 import type { SafetyCollection } from '../collection/safety';
+import type { MineralCategory, MineralClass } from '../enum/mineral';
 
 export type MineralID = Brand< string, 'mineralID' >;
 
+export type MineralClassification = Collection< {
+  category: Distinct< MineralCategory >;
+  class: Distinct< MineralClass >;
+  imaSymbol: Distinct< string >;
+  strunz8: Distinct< string >;
+  strunz9: Distinct< string >;
+  dana: Distinct< string >;
+  lapis: Distinct< string >;
+} >;
+
 export type SingleMineral = Collection< {
   descriptive: DescriptiveCollection;
+  classification: MineralClassification;
+  composition: CompositionCollection;
   generic?: GenericCollection;
   abundance?: AbundanceCollection;
   physics?: PhysicsCollection;
@@ -21,7 +35,7 @@ export type SingleMineral = Collection< {
 } >;
 
 export type Mineral = Expand< MetaData & SingleMineral & {
-  forms?: FormCollection< SingleMineral >; 
+  forms?: FormCollection< SingleMineral >;
 } >;
 
 export type MineralEntity = Collection< {
---
+++ types/enum/mineral.ts
@@ -0,0 +1,34 @@
+export enum MineralCategory {
+  ELEMENTS = 'elements',
+  SULFIDES = 'sulfides',
+  HALIDES = 'halides',
+  OXIDES = 'oxides',
+  CARBONATES = 'carbonates',
+  BORATES = 'borates',
+  SULFATES = 'sulfates',
+  PHOSPHATES = 'phosphates',
+  SILICATES = 'silicates',
+  ORGANIC = 'organic',
+  UNKNOWN = 'unknown'
+};
+
+export enum MineralClass {
+  ELEMENTS = 'elements',
+  SULFIDES = 'sulfides',
+  SULFOSALTS = 'sulfosalts',
+  HALIDES = 'halides',
+  OXIDES = 'oxides',
+  HYDROXIDES = 'hydroxides',
+  ARSENITES = 'arsenites',
+  CARBONATES = 'carbonates',
+  NITRATES = 'nitrates',
+  BORATES = 'borates',
+  SULFATES = 'sulfates',
+  CHROMATES = 'chromates',
+  MOLYBDATES = 'molybdates',
+  PHOSPHATES = 'phosphates',
+  ARSENATES = 'arsenates',
+  ORGANIC = 'organic',
+  SILICATES = 'silicates',
+  UNKNOWN = 'unknown'
+};
