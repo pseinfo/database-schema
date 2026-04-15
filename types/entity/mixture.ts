651ab3fd454c74de803953334f5f0834164b4784
komed3 <pk97@outlook.de>
2026-04-16 00:15:04 +0200

    mixture classification + enums

--- types/entity/mixture.ts
+++ types/entity/mixture.ts
@@ -10,13 +10,20 @@ import type { GenericCollection } from '../collection/generic';
 import type { PhysicsCollection } from '../collection/physics';
 import type { SafetyCollection } from '../collection/safety';
 import type { Phase } from '../enum/generic';
+import type { MixtureCategory, MixtureHomogeneity, MixtureProperty, MixtureType } from '../enum/mixture';
 
 export type MixtureID = Brand< string, 'mixtureID' >;
 
 export type MixtureClassification = Collection< {
+  type: Distinct< MixtureType >;
+  homogeneity: Distinct< MixtureHomogeneity >;
+  category: Distinct< MixtureCategory >;
   phase: Distinct< Phase >;
+  mediumPhase: Distinct< Phase >;
+  dispersedPhase: Distinct< Phase >;
   radioactive: Distinct< boolean >;
   synthetic: Distinct< boolean >;
+  properties: Distinct< MixtureProperty[] >;
 } >;
 
 export type SingleMixture = Collection< {
