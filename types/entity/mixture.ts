32842466ef1eee828be65f50f43329e97f813885
komed3 <pk97@outlook.de>
2026-04-15 23:43:19 +0200

    Update mixture.ts

--- types/entity/mixture.ts
+++ types/entity/mixture.ts
@@ -1,4 +1,10 @@
-import { Collection } from '../abstract/collection';
+import type { Expand } from 'devtypes/types/util';
+import type { Collection } from '../abstract/collection';
+import type { MetaData } from '../abstract/util';
+
+export type SingleMixture = Collection< {} >;
+
+export type Mixture = Expand< MetaData & SingleMixture >;
 
 export type MixtureEntity = Collection< {
   [ key: MixtureID ]: Mixture;
