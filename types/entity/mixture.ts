6873cef9a1e8051766bab06b9f6e6001dd7b8226
komed3 <pk97@outlook.de>
2026-04-15 23:45:29 +0200

    add collections to mixture entity

--- types/entity/mixture.ts
+++ types/entity/mixture.ts
@@ -1,10 +1,27 @@
 import type { Brand, Expand } from 'devtypes/types/util';
 import type { Collection } from '../abstract/collection';
 import type { MetaData } from '../abstract/util';
+import type { AbundanceCollection } from '../collection/abundance';
+import type { ChemistryCollection } from '../collection/chemistry';
+import type { CompositionCollection } from '../collection/composition';
+import type { CrystallographyCollection } from '../collection/crystallography';
+import type { DescriptiveCollection } from '../collection/descriptive';
+import type { GenericCollection } from '../collection/generic';
+import type { PhysicsCollection } from '../collection/physics';
+import type { SafetyCollection } from '../collection/safety';
 
 export type MixtureID = Brand< string, 'mixtureID' >;
 
-export type SingleMixture = Collection< {} >;
+export type SingleMixture = Collection< {
+  descriptive: DescriptiveCollection;
+  composition: CompositionCollection;
+  generic?: GenericCollection;
+  abundance?: AbundanceCollection;
+  physics?: PhysicsCollection;
+  crystallography?: CrystallographyCollection;
+  chemistry?: ChemistryCollection;
+  safety?: SafetyCollection;
+} >;
 
 export type Mixture = Expand< MetaData & SingleMixture >;
 
