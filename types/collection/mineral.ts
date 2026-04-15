0c2758dd1f68ea2bc12810ccec8b76e19400caa8
komed3 <pk97@outlook.de>
2026-04-15 19:37:01 +0200

    Update mineral.ts

--- types/collection/mineral.ts
+++ types/collection/mineral.ts
@@ -1,8 +1,10 @@
 import type { Collection, Single } from '../abstract/collection';
-import type { NumberProperty, PrimitiveProperty } from '../abstract/property';
+import type { NumberProperty } from '../abstract/property';
+import type { LangGroup } from '../abstract/util';
 import type { PhysicalQuantity } from '../enum/util';
 
 export type MineralCollection = Collection< {
   formulaMass?: Single< NumberProperty< PhysicalQuantity.MOLAR_MASS > >;
-  color?: Single< PrimitiveProperty< string > >;
+  color?: LangGroup;
+  streak?: LangGroup;
 } >;
