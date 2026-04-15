af38639aa5f00b80e314738d0de9f9f8b219b01a
komed3 <pk97@outlook.de>
2026-04-15 23:55:04 +0200

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,14 +1,19 @@
 import type { Collection } from './abstract/collection';
 import type { ReferenceCollection } from './abstract/reference';
 import type { UnitCollection } from './abstract/unit';
+import type { MetaData } from './abstract/util';
 import type { CompoundEntity } from './entity/compound';
 import type { ElementEntity } from './entity/element';
 import type { MineralEntity } from './entity/mineral';
 import type { MixtureEntity } from './entity/mixture';
 import type { NuclideEntity } from './entity/nuclide';
 
+export type DBMetaCollection = Collection< MetaData & {
+  stats: any;
+} >;
+
 export type Database = Collection< {
-  meta: any;
+  meta: DBMetaCollection;
   data: Collection< {
     elements: ElementEntity;
     nuclides: NuclideEntity;
