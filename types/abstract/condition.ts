2b4a4ed0432be536f224009405b5063ad5bad255
komed3 <pk97@outlook.de>
2026-04-15 10:06:50 +0200

    change stp to enum

--- types/abstract/condition.ts
+++ types/abstract/condition.ts
@@ -1,9 +1,7 @@
 import type { Primitive } from 'devtypes/types/primitive';
-import { stp } from '../../config/stp';
+import type { StandardCondition } from '../enum/util';
 import type { PhysicalQuantity } from './unit';
 import type { Value } from './value';
-
-export type StandardCondition = keyof typeof stp;
 
 export type Conditions<
   Q extends PhysicalQuantity = PhysicalQuantity,
