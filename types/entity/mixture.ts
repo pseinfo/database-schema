56774f4531c171f0a74fe4af4918bfe2f8051dfc
komed3 <pk97@outlook.de>
2026-04-16 11:28:41 +0200

    Update mixture.ts

--- types/entity/mixture.ts
+++ types/entity/mixture.ts
@@ -1,16 +1,9 @@
 import type { Brand, Expand } from 'devtypes/types/util';
 import type { Collection, Distinct } from '../abstract/collection';
 import type { MetaData } from '../abstract/util';
-import type { AbundanceCollection } from '../collection/abundance';
-import type { ChemistryCollection } from '../collection/chemistry';
-import type { CompositionCollection } from '../collection/composition';
-import type { CrystallographyCollection } from '../collection/crystallography';
-import type { DescriptiveCollection } from '../collection/descriptive';
-import type { GenericCollection } from '../collection/generic';
-import type { PhysicsCollection } from '../collection/physics';
-import type { SafetyCollection } from '../collection/safety';
 import type { Phase } from '../enum/generic';
 import type { MixtureCategory, MixtureHomogeneity, MixtureProperty, MixtureType } from '../enum/mixture';
+import type { Composite } from './composite';
 
 export type MixtureID = Brand< string, 'mixtureID' >;
 
@@ -26,17 +19,7 @@ export type MixtureClassification = Collection< {
   properties: Distinct< MixtureProperty[] >;
 } >;
 
-export type SingleMixture = Collection< {
-  descriptive: DescriptiveCollection;
-  classification: MixtureClassification;
-  composition: CompositionCollection;
-  generic?: GenericCollection;
-  abundance?: AbundanceCollection;
-  physics?: PhysicsCollection;
-  crystallography?: CrystallographyCollection;
-  chemistry?: ChemistryCollection;
-  safety?: SafetyCollection;
-} >;
+export type SingleMixture = Composite< MixtureClassification >;
 
 export type Mixture = Expand< MetaData & SingleMixture >;
 
