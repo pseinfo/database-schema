0036d89e76cb5898b48b3ee6078fc519e408b4d1
komed3 <pk97@outlook.de>
2026-04-16 12:20:01 +0200

    add SchemaGenerator class

--- scripts/schema.mjs
+++ scripts/schema.mjs
@@ -1,1 +1,21 @@
 #!/usr/bin/env node
+
+class SchemaGenerator {
+
+  FILES = {
+    FINAL: 'src/schema.json',
+    TSCONFIG: 'tsconfig.json',
+    PACKAGE: 'package.json',
+    TYPES_ENTRY: 'types/index.ts'
+  };
+
+  CONFIG = {
+    HASH_PREFIX: 'pseinfo@',
+    HASH_LENGTH: 8,
+    FORBIDDEN_PARENT_KEYS: new Set( [ 'definitions', '$defs', 'properties', 'patternProperties', 'dependencies' ] ),
+    MIN_SAVINGS_THRESHOLD: 40,
+    MIN_OCCURRENCES: 3,
+    MIN_NODE_SIZE: 30
+  };
+
+}
