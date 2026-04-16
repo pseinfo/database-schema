e3466a740b17d9cf8ca2ec137dff049745c10c6d
komed3 <pk97@outlook.de>
2026-04-16 11:18:31 +0200

    Update element.ts

--- types/entity/element.ts
+++ types/entity/element.ts
@@ -2,16 +2,9 @@ import type { Expand } from 'devtypes/types/util';
 import type { Collection, Distinct } from '../abstract/collection';
 import type { FormCollection } from '../abstract/form';
 import type { MetaData } from '../abstract/util';
-import type { AbundanceCollection } from '../collection/abundance';
-import type { AtomicsCollection } from '../collection/atomics';
-import type { ChemistryCollection } from '../collection/chemistry';
-import type { CrystallographyCollection } from '../collection/crystallography';
-import type { DescriptiveCollection } from '../collection/descriptive';
-import type { GenericCollection } from '../collection/generic';
-import type { PhysicsCollection } from '../collection/physics';
-import type { SafetyCollection } from '../collection/safety';
 import type { ElementBlock, ElementGroup, ElementProperty, ElementSet, ElementSymbol } from '../enum/element';
 import type { Phase, PTColumn, PTPeriod } from '../enum/generic';
+import type { Substance } from './substance';
 
 export type ElementClassification = Collection< {
   symbol: Distinct< string >;
@@ -27,17 +20,7 @@ export type ElementClassification = Collection< {
   properties: Distinct< ElementProperty[] >;
 } >;
 
-export type SingleElement = {
-  descriptive: DescriptiveCollection;
-  classification: ElementClassification;
-  generic?: GenericCollection;
-  abundance?: AbundanceCollection;
-  atomics?: AtomicsCollection;
-  physics?: PhysicsCollection;
-  crystallography?: CrystallographyCollection;
-  chemistry?: ChemistryCollection;
-  safety?: SafetyCollection;
-};
+export type SingleElement = Substance< ElementClassification >;
 
 export type Element = Expand< MetaData & SingleElement & {
   forms?: FormCollection< SingleElement >;
