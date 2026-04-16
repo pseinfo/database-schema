677ae7ae7e423d6505b356986ca0e79b986f3029
komed3 <pk97@outlook.de>
2026-04-16 11:27:02 +0200

    remove meta data from abstract substance

--- types/entity/substance.ts
+++ types/entity/substance.ts
@@ -1,6 +1,4 @@
-import type { Expand } from 'devtypes/types/util';
 import type { Collection } from '../abstract/collection';
-import type { MetaData } from '../abstract/util';
 import type { AbundanceCollection } from '../collection/abundance';
 import type { ChemistryCollection } from '../collection/chemistry';
 import type { CrystallographyCollection } from '../collection/crystallography';
@@ -9,7 +7,7 @@ import type { GenericCollection } from '../collection/generic';
 import type { PhysicsCollection } from '../collection/physics';
 import type { SafetyCollection } from '../collection/safety';
 
-export type Substance< C extends Collection< unknown > > = Expand< MetaData & {
+export type Substance< C extends Collection< unknown > > = Collection< {
   descriptive: DescriptiveCollection;
   classification: C;
   generic?: GenericCollection;
