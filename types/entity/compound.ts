0727fbc7411b38eb4c8d0fb898c6d01f7a4be021
komed3 <pk97@outlook.de>
2026-04-16 11:24:03 +0200

    Update compound.ts

--- types/entity/compound.ts
+++ types/entity/compound.ts
@@ -2,16 +2,9 @@ import type { Brand, Expand } from 'devtypes/types/util';
 import type { Collection, Distinct } from '../abstract/collection';
 import type { FormCollection } from '../abstract/form';
 import type { MetaData } from '../abstract/util';
-import type { AbundanceCollection } from '../collection/abundance';
-import type { ChemistryCollection } from '../collection/chemistry';
-import type { CompositionCollection } from '../collection/composition';
-import type { CrystallographyCollection } from '../collection/crystallography';
-import type { DescriptiveCollection } from '../collection/descriptive';
-import type { GenericCollection } from '../collection/generic';
-import type { PhysicsCollection } from '../collection/physics';
-import type { SafetyCollection } from '../collection/safety';
 import type { CompoundCategory, CompoundProperty, CompoundUnion } from '../enum/compound';
 import type { Phase } from '../enum/generic';
+import type { Composite } from './composite';
 
 export type CompoundID = Brand< string, 'compoundID' >;
 
@@ -25,17 +18,7 @@ export type CompoundClassification = Collection< {
   properties: Distinct< CompoundProperty[] >;
 } >;
 
-export type SingleCompound = Collection< {
-  descriptive: DescriptiveCollection;
-  classification: CompoundClassification;
-  composition: CompositionCollection;
-  generic?: GenericCollection;
-  abundance?: AbundanceCollection;
-  physics?: PhysicsCollection;
-  crystallography?: CrystallographyCollection;
-  chemistry?: ChemistryCollection;
-  safety?: SafetyCollection;
-} >;
+export type SingleCompound = Composite< CompoundClassification >;
 
 export type Compound = Expand< MetaData & SingleCompound & {
   forms?: FormCollection< SingleCompound >;
