16980f5b73b57d3d2f8d521f1311c1001f29942d
komed3 <pk97@outlook.de>
2026-04-20 00:39:19 +0200

    fix phys. quantity for abundance collection

--- types/collection/abundance.ts
+++ types/collection/abundance.ts
@@ -17,30 +17,30 @@ export type AbundanceCollection = Collection< {
   naturalOccurrence?: Single< PrimitiveProperty< NaturalOccurrence > >;
 
   /** The estimated mass or molar fraction of the substance in the observable universe. */
-  universeAbundance?: Single< NumberProperty< 'quantity' > >;
+  universeAbundance?: Single< NumberProperty< 'massFraction' > >;
   /** The estimated concentration of the substance within the entirety of the solar system. */
-  solarSystemAbundance?: Single< NumberProperty< 'quantity' > >;
+  solarSystemAbundance?: Single< NumberProperty< 'massFraction' > >;
   /** The concentration of the substance found in the Sun's photosphere or corona. */
-  sunAbundance?: Single< NumberProperty< 'quantity' > >;
+  sunAbundance?: Single< NumberProperty< 'massFraction' > >;
   /** The characteristic concentration of the substance found in CI chondrite meteorites. */
-  meteoriteAbundance?: Single< NumberProperty< 'quantity' > >;
+  meteoriteAbundance?: Single< NumberProperty< 'massFraction' > >;
 
   /** The average mass fraction of the substance found in the Earth's continental crust. */
-  crustalAbundance?: Single< NumberProperty< 'quantity' > >;
+  crustalAbundance?: Single< NumberProperty< 'massFraction' > >;
   /** The average concentration of the substance within the global oceanic volume. */
-  oceanAbundance?: Single< NumberProperty< 'quantity' > >;
+  oceanAbundance?: Single< NumberProperty< 'massFraction' > >;
   /** The typical concentration of the substance found specifically in seawater. */
-  seaAbundance?: Single< NumberProperty< 'quantity' > >;
+  seaAbundance?: Single< NumberProperty< 'massFraction' > >;
   /** The concentration of the substance found in terrestrial freshwater streams. */
-  streamAbundance?: Single< NumberProperty< 'quantity' > >;
+  streamAbundance?: Single< NumberProperty< 'massFraction' > >;
   /** The volume or mass fraction of the substance within the planetary atmosphere. */
-  atmosphereAbundance?: Single< NumberProperty< 'quantity' > >;
+  atmosphereAbundance?: Single< NumberProperty< 'massFraction' > >;
 
   /** The average mass percentage of the substance found within the human body. */
-  humanAbundance?: Single< NumberProperty< 'quantity' > >;
+  humanAbundance?: Single< NumberProperty< 'massFraction' > >;
 
   /** The relative typical concentration of the substance found in the Earth's mineralogy. */
-  mineralAbundance?: Single< NumberProperty< 'quantity' > >;
+  mineralAbundance?: Single< NumberProperty< 'massFraction' > >;
   /** The concentration of the substance in economically exploitable geological formations. */
-  oreAbundance?: Single< NumberProperty< 'quantity' > >;
+  oreAbundance?: Single< NumberProperty< 'massFraction' > >;
 } >;
