1d85d41e6d13228eff3268ac9001ca041823b2d5
komed3 <pk97@outlook.de>
2026-04-30 03:40:03 +0200

    implement reworked measurement uncertainty

---
+++ enum/base/uncertainty.ts
@@ -0,0 +1,16 @@
+export enum UncertaintyType {
+  ABSOLUTE = 'absolute',
+  RELATIVE = 'relative'
+};
+
+export enum UncertaintyShape {
+  SYMMETRICAL = 'symmetrical',
+  ASYMMETRICAL = 'asymmetrical',
+  INTERVAL = 'interval',
+  UNKNOWN = 'unknown'
+};
+
+export enum ConfidenceType {
+  LEVEL = 'level',
+  SIGMA = 'sigma'
+};
---
+++ model/base/uncertainty.ts
@@ -0,0 +1,24 @@
+import type { Brand, Expand } from 'devtypes/types/util';
+import type { ConfidenceType, UncertaintyShape, UncertaintyType } from '../../enum/base/uncertainty';
+
+type BaseUncertainty< T extends UncertaintyType > = Expand< Brand< {
+  shape:
+    | { type: UncertaintyShape.SYMMETRICAL, value: number }
+    | { type: UncertaintyShape.ASYMMETRICAL, plus: number, minus: number }
+    | { type: UncertaintyShape.INTERVAL, min: number, max: number }
+    | { type: UncertaintyShape.UNKNOWN };
+  confidence?:
+    | { type: ConfidenceType.LEVEL, value: number }
+    | { type: ConfidenceType.SIGMA, value: number };
+  meta?: {
+    source?: string;
+    method?: string;
+  };
+  note?: string;
+}, T, 'type', true > >;
+
+export type AbsoluteUncertainty = BaseUncertainty< UncertaintyType.ABSOLUTE >;
+
+export type RelativeUncertainty = BaseUncertainty< UncertaintyType.RELATIVE >;
+
+export type Uncertainty = AbsoluteUncertainty | RelativeUncertainty;
