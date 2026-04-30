538f3af0c7ef78c8b09a2c939da403581bf77ea8
komed3 <pk97@outlook.de>
2026-04-30 10:58:03 +0200

    implement history collection

---
+++ enum/collection/history.ts
@@ -0,0 +1,16 @@
+export enum EventType {
+  DISCOVERY = 'discovery',
+  FIRST_OBSERVATION = 'firstObservation',
+  ISOLATION = 'isolation',
+  SYNTHESIS = 'synthesis',
+  NAMING = 'naming',
+  CONFIRMATION = 'confirmation',
+  CHARACTERIZATION = 'characterization'
+};
+
+export enum EventTimeType {
+  DATE = 'date',
+  YEAR = 'year',
+  RANGE = 'range',
+  TEXT = 'text'
+};
---
+++ model/collection/history.ts
@@ -0,0 +1,58 @@
+import type { Brand, Expand } from 'devtypes/types/util';
+import type { CountryCode } from '../../enum/base/locale';
+import type { EventTimeType, EventType } from '../../enum/collection/history';
+import type { Collection, Distinct } from '../base/modifier';
+import type { IsoDate } from '../base/primitive';
+import type { OrgId } from '../registry/organization';
+import type { PersonId } from '../registry/person';
+import type { RefId } from '../registry/reference';
+
+type BaseEvent< E extends EventType > = Brand< {
+  time:
+    | { type: EventTimeType.DATE, date: IsoDate }
+    | { type: EventTimeType.YEAR, year: number; approximate?: boolean }
+    | { type: EventTimeType.RANGE, fromYear: number; toYear: number }
+    | { type: EventTimeType.TEXT, text: string };
+  actors?: PersonId[];
+  organizations?: OrgId[];
+  locations?: {
+    country?: CountryCode;
+    place?: string;
+  }[];
+  notes?: string;
+  references?: RefId[];
+}, E, 'type', true >;
+
+type MethodField = {
+  method: string;
+};
+
+type NamingFields = {
+  name: string;
+  namedAfter?: {
+    persons?: PersonId[];
+    organizations?: OrgId[];
+    literal?: string;
+  };
+};
+
+export type DiscoveryEvent = Expand< BaseEvent< EventType.DISCOVERY > & MethodField >;
+export type FirstObservationEvent = Expand< BaseEvent< EventType.FIRST_OBSERVATION > >;
+export type IsolationEvent = Expand< BaseEvent< EventType.ISOLATION > >;
+export type SynthesisEvent = Expand< BaseEvent< EventType.SYNTHESIS > & MethodField >;
+export type NamingEvent = Expand< BaseEvent< EventType.NAMING > & NamingFields >;
+export type ConfirmationEvent = Expand< BaseEvent< EventType.CONFIRMATION > >;
+export type CharacterizationEvent = Expand< BaseEvent< EventType.CHARACTERIZATION > >;
+
+export type HistoryEvent =
+  | DiscoveryEvent
+  | FirstObservationEvent
+  | IsolationEvent
+  | SynthesisEvent
+  | NamingEvent
+  | ConfirmationEvent
+  | CharacterizationEvent;
+
+export type HistoryCollection = Collection< {
+  events?: Distinct< HistoryEvent >[];
+} >;
