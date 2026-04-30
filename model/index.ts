49eef596e9290dfbb4e67af265fdd4dc3106a1b9
komed3 <pk97@outlook.de>
2026-04-30 09:45:20 +0200

    mount registries to main DB type

--- model/index.ts
+++ model/index.ts
@@ -1,1 +1,16 @@
-export type Database = {};
+import type { Collection, Group } from './base/modifier';
+import type { BlobRegistry } from './registry/blob';
+import type { OrganizationRegistry } from './registry/organization';
+import type { PersonRegistry } from './registry/person';
+import type { ReferenceRegistry } from './registry/reference';
+import type { DBMeta } from './utility/meta';
+
+export type Database = Collection< {
+  meta: DBMeta;
+  registries: Group< {
+    blobs: BlobRegistry;
+    references: ReferenceRegistry;
+    orgs: OrganizationRegistry;
+    persons: PersonRegistry;
+  } >;
+} >;
