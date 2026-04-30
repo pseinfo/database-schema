b2f73d70188e299d88c3da779d37d5c01944f965
komed3 <pk97@outlook.de>
2026-04-30 04:08:52 +0200

    Update factory.ts

--- model/base/factory.ts
+++ model/base/factory.ts
@@ -1,9 +1,10 @@
 import type { Brand, Expand } from 'devtypes/types/util';
 import type { EntityType, RegistryType } from '../../enum/registry/system';
+import type { Collection } from './modifier';
 import type { Struct } from './primitives';
 
 export type Factory<
   T extends EntityType | RegistryType,
-  C extends unknown,
+  C extends Collection< unknown >,
   K extends Struct< string | number >
 > = Expand< Brand< K & { data: C }, T, 'type', true > >;
