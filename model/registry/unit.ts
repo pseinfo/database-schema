87874c79f05e4c555e0828553ec263c750711c2f
komed3 <pk97@outlook.de>
2026-04-30 10:02:15 +0200

    SI dimensions + prefixes

--- model/registry/unit.ts
+++ model/registry/unit.ts
@@ -1,6 +1,30 @@
+import type { Brand, Expand } from 'devtypes/types/util';
+import type { RegistryType } from '../../enum/registry/system';
+import type { MeasurementSystem } from '../../enum/registry/unit';
+import type { Factory } from '../base/factory';
+import type { Collection, Distinct } from '../base/modifier';
+import type { LangGroup } from '../base/primitives';
+
+import ValidUnits from '../../config/units';
+
+export type NoUnit = never;
+
 export type DimensionVector = readonly [
   T: number, L: number, M: number, I: number, Θ: number, N: number, J: number
 ];
+
+export type SIDimension = ( typeof SIDimension )[ number ];
+export const SIDimension = [
+    'time', 'length', 'mass', 'electricCurrent', 'temperature',
+    'amountOfSubstance', 'luminousIntensity'
+] as const;
+
+export type SIPrefix = keyof typeof SIPrefix;
+export const SIPrefix = {
+  Y: 1e24, Z: 1e21, E: 1e18, P: 1e15, T: 1e12, G: 1e9, M: 1e6,
+  k: 1e3, h: 1e2, da: 1e1, d: 1e-1, c: 1e-2, m: 1e-3, µ: 1e-6,
+  n: 1e-9, p: 1e-12, f: 1e-15, a: 1e-18, z: 1e-21, y: 1e-24
+} as const;
 
 export type UnitConfig = Record< string, {
   symbol: string;
