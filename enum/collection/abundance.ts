552b6b26abfed400c5c3e356751480f218ac9981
komed3 <pk97@outlook.de>
2026-04-30 10:43:05 +0200

    implement abundance collection

---
+++ enum/collection/abundance.ts
@@ -0,0 +1,21 @@
+export enum NaturalOccurrence {
+  PRIMORDIAL = 'primordial',
+  DECAY_PRODUCT = 'decayProduct',
+  SYNTHETIC = 'synthetic'
+};
+
+export enum AbundanceType {
+  UNIVERSE = 'universe',
+  SOLAR_SYSTEM = 'solarSystem',
+  METEORITES = 'meteorites',
+  EARTH_CRUST = 'earthCrust',
+  EARTH_MANTLE = 'earthMantle',
+  OCEAN = 'ocean',
+  SEAWATER = 'seawater',
+  STREAMS = 'streams',
+  ATMOSPHERE = 'atmosphere',
+  MINERALS = 'minerals',
+  ORES = 'ores',
+  HUMAN_BODY = 'humanBody',
+  BIOLOGY = 'biology'
+};
---
+++ model/collection/abundance.ts
@@ -0,0 +1,8 @@
+import type { AbundanceType, NaturalOccurrence } from '../../enum/collection/abundance';
+import type { Collection, Mapping, One, OneOrMany } from '../base/modifier';
+import type { NumberProperty, PrimitiveProperty } from '../base/property';
+
+export type AbundanceCollection = Collection< {
+  naturalOccurrence?: One< PrimitiveProperty< NaturalOccurrence > >;
+  abundances?: Mapping< AbundanceType, OneOrMany< NumberProperty< 'massFraction' > > >;
+} >;
