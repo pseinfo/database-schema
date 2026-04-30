ee2f70371ce8dbd43f0d7fd0bc883d150b56edf5
komed3 <pk97@outlook.de>
2026-04-30 10:05:05 +0200

    Update index.ts

--- model/index.ts
+++ model/index.ts
@@ -3,12 +3,14 @@ import type { BlobRegistry } from './registry/blob';
 import type { OrganizationRegistry } from './registry/organization';
 import type { PersonRegistry } from './registry/person';
 import type { ReferenceRegistry } from './registry/reference';
+import type { UnitRegistry } from './registry/unit';
 import type { DBMeta } from './utility/meta';
 
 export type Database = Collection< {
   meta: DBMeta;
   registries: Group< {
     blobs: BlobRegistry;
+    units: UnitRegistry;
     references: ReferenceRegistry;
     orgs: OrganizationRegistry;
     persons: PersonRegistry;
