ff77a4da1440ad1829762c0108b655b84b7e794c
komed3 <pk97@outlook.de>
2026-04-30 04:01:44 +0200

    implement org registry infrastructure

--- model/registry/organization.ts
+++ model/registry/organization.ts
@@ -1,6 +1,10 @@
-import { CountryCode } from '../../enum/base/locale';
-import { OrganizationType } from '../../enum/registry/organization';
-import { RefId } from './reference';
+import type { Brand } from 'devtypes/types/util';
+import type { CountryCode } from '../../enum/base/locale';
+import type { OrganizationType } from '../../enum/registry/organization';
+import type { RegistryType } from '../../enum/registry/system';
+import type { Factory } from '../base/factory';
+import type { Collection, Distinct } from '../base/modifier';
+import type { RefId } from './reference';
 
 export type Organization = {
   name: string;
@@ -10,3 +14,13 @@ export type Organization = {
   weblinks?: Weblinks;
   references?: RefId[];
 };
+
+export type OrgId = Brand< string, 'orgId' >;
+
+export type OrganizationRegistry = Collection< {
+  [ K in OrgId ]: Distinct< Organization >;
+} >;
+
+export type OrganizationFactory = Factory< RegistryType.ORGANIZATION, Organization, {
+  orgId: OrgId
+} >;
